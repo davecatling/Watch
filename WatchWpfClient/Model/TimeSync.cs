3d14a6f36cb9802754645141c837162e121a5dc7
David Catling <davecatling@gmail.com>
2022-07-31 00:12:12 +1000

    TimeSyncs populating and culling.

---
+++ WatchWpfClient/Model/TimeSync.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchWpfClient.Model
+{
+    public class TimeSync : IWatchItem
+    {
+        public string Id { get; private set; }
+        public string Text { get; private set; }
+        public DateTime Time { get; private set; }
+        public int Server { get; private set; }
+        private readonly int _illegalServer = 0;
+
+        public TimeSync()
+        {
+            Id = Guid.NewGuid().ToString();
+            while (Server == _illegalServer)
+                Server = new Random().Next(1, 12);
+            Text = $"Synced with world server #{Server} - difference: {new Random().NextDouble()}";
+            Time = DateTime.Now;
+        }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public TimeSync(TimeSync prevTimeSync) : base()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+            _illegalServer = prevTimeSync.Server;
+        }
+    }
+
+}
---
+++ WatchWpfClient/Model/WatchApp.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WatchWpfClient.Model
+{
+    public class WatchApp
+    {
+        public delegate void ItemAddedOrRemovedEventHandler(object sender, ItemAddedOrRemovedEventArgs args);
+        public event ItemAddedOrRemovedEventHandler? ItemAddedOrRemoved;
+        public List<TimeSync>? TimeSyncs { get; private set; }
+
+        private readonly Timer _syncTimer;
+
+        public WatchApp()
+        {
+            TimeSyncs = new List<TimeSync>();
+            _syncTimer = new Timer(new Random().NextInt64(2000, 3000));
+            _syncTimer.Elapsed += SyncTimer_Elapsed;
+            _syncTimer.Start();
+        }
+
+        private void SyncTimer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            AddNewSync();
+            _syncTimer.Interval = new Random().NextInt64(TimeSyncs!.Count < 3 ? 2000 : 10000, TimeSyncs.Count < 3 ? 10000 : 60000);
+        }
+
+        private void AddNewSync()
+        {
+            TimeSync newTimeSync;
+            if (TimeSyncs!.Count > 0)
+                newTimeSync = new TimeSync(TimeSyncs.Last());
+            else
+                newTimeSync = new TimeSync();
+            TimeSyncs.Add(newTimeSync);
+            ItemAddedOrRemoved?.Invoke(this, new ItemAddedOrRemovedEventArgs(newTimeSync, ChangeType.Added));
+            CullTimeSyncs();
+        }
+
+        private void CullTimeSyncs()
+        {
+            while (TimeSyncs!.Count > 10)
+            {
+                var oldestSync = TimeSyncs.MinBy(ts => ts.Time);
+                if (oldestSync != null)
+                {
+                    TimeSyncs.Remove(oldestSync);
+                    ItemAddedOrRemoved?.Invoke(this, new ItemAddedOrRemovedEventArgs(oldestSync, ChangeType.Removed));
+                }
+            }
+        }
+
+        public class ItemAddedOrRemovedEventArgs : EventArgs
+        {
+            public IWatchItem Item { get; private set; }
+            public ChangeType ChangeType { get; private set; }
+            public ItemAddedOrRemovedEventArgs(IWatchItem watchItem, ChangeType changeType)
+            {
+                Item = watchItem;
+                ChangeType = changeType;
+            }
+        }
+
+        public enum ChangeType
+        {
+            Added,
+            Removed
+        }
+    }
+   
+}
