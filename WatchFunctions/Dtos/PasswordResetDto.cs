ebef9324b056dc8d7734b639ce5da597e6ef3745
David Catling <davecatling@gmail.com>
2022-11-29 05:24:50 +1100

    Password reset function and dtos created.

---
+++ WatchFunctions/Dtos/PasswordResetDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WatchFunctions.Dtos
+{
+    public class PasswordResetDto
+    {
+        public string ChannelNumber { get; set; }
+        public string Handle { get; set; }
+        public string Password { get; set; }
+        public byte[] Signature { get; set; }
+    }
+}
