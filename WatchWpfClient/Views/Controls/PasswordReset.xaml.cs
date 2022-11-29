7224f240aa159b14e68afdb9221ceaa2c0e15521
David Catling <davecatling@gmail.com>
2022-11-30 05:37:13 +1100

    Password reset view created and showable.

---
+++ WatchWpfClient/Views/Controls/PasswordReset.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace WatchWpfClient.Views.Controls
+{
+    public partial class PasswordReset : UserControl
+    {
+        public PasswordReset()
+        {
+            InitializeComponent();
+        }
+    }
+}
